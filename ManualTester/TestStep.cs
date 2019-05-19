644993c526511cb0dabdc3bddff04821776620ec
Dominika <ddd.wilk@gmail.com>
2019-05-19 20:34:28 +0100

    TestSteps now have default values

--- ManualTester/TestStep.cs
+++ ManualTester/TestStep.cs
@@ -15,20 +15,19 @@ namespace WindowsFormsApp1
             Wait
         }
 
-        public TestDatabase.TestAction actionIfSucceeded;   //[TODO] What to do if step succeeded
-        public TestDatabase.TestAction actionIfFailed;      //What to do if confirmation is absent
-        public StepType testStepType;                       //[TODO] What is the type of step (ex. Tap, Wait, Command)
+        public TestDatabase.TestAction actionIfSucceeded = TestDatabase.TestAction.Stop;    //[TODO] What to do if step succeeded
+        public TestDatabase.TestAction actionIfFailed = TestDatabase.TestAction.Stop;       //What to do if confirmation is absent
+        public StepType testStepType;                                                       //TODO more types
 
-        public string testStepID;                           //Name of step. Needs to be unique.
-        public string argument;                             //[TODO] For now, useful only for steps of type Command - Provide command here
-        public List<string> conditionLog;                   //[TODO] Step will be executed only if at least one of the conditionLogs is present. If the condition is absent, the step ends with success.
-        public List<string> confirmationLog;                //Step ends with a success if at least one of the confirmationLog is present. If the confirmation is absent, the stem ends with fail.
+        public string testStepID;                                                           //Name of step. Needs to be unique.
+        public List<string> conditionLog;                                                   //[TODO] Step will be executed only if at least one of the conditionLogs is present. If the condition is absent, the step ends with success.
+        public List<string> confirmationLog;                                                //Step ends with a success if at least one of the confirmationLog is present. If the confirmation is absent, the stem ends with fail.
 
-        public int waitTime;                               //[TODO] Time in miliseconds. Amount of time the program will wait before executing step.
-        public int terminationTime;                         //Time in miliseconds. If no confirmation arrived after this time, step is recognized as failed. Test Logic checks every 500+ ms
+        public int waitTime = 5000;                                                         //[TODO] Time in miliseconds. Amount of time the program will wait before executing step.
+        public int terminationTime = 10000;                                                 //Time in miliseconds. If no confirmation arrived after this time, step is recognized as failed. Test Logic checks every 500+ ms
 
-        public double posY;                                    //Y position of tap (needs to be provided only for steps of type Tap) [TODO] Support for 1. Different resolutions 2. Different aspect ratios 3. Safe Area sesnivity
-        public double posX;                                    //X position of tap (needs to be provided only for steps of type Tap) [TODO] 1. Different resolutions 2. Different aspect ratios 3. Safe Area sesnivity
+        public double posY = 0;                                                             //Range 0-1 [TODO] Support for 1. Different aspect ratios 2. Safe Area sesnivity
+        public double posX = 0;                                                             //Range 0-1 [TODO] Support for 1. Different aspect ratios 2. Safe Area sesnivity
 
     }
 }
